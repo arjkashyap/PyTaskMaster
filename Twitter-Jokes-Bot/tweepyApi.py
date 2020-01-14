fdbfdb8a5bdca65702eeae3875d529c9a81e01a8
Arjun <arj.kashyap123@gmail.com>
2020-01-15 03:08:03 +0530

    Tweepy API for Publish alternative

--- Twitter-Jokes-Bot/tweepyApi.py
+++ Twitter-Jokes-Bot/tweepyApi.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python3.6
 # Alternate module for tweet publish
 # Incase publish.py does not work
 
-import tweepy
 import config
+import tweepy
 
-# Auth tokens
+# Authentication to twitter
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+
+# Api object
+api = tweepy.API(auth)
+
+api.update_status("Hello Twitter. Lets see how many Days I can go without getting blocked.. ")

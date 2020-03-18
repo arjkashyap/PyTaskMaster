133105dfa84ea1a5b6981f79e85d079389d27948
Arjun <arj.kashyap123@gmail.com>
2020-03-19 00:26:43 +0530

    Bar graph plotter complete

--- COVID-19-Tracker/plot_graph.py
+++ COVID-19-Tracker/plot_graph.py
@@ -8,7 +8,6 @@ Daily covid-19 data and progress tracker.
 """
 
 import csv
-import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt 
 import os
@@ -18,13 +17,13 @@ from get_data import current_data, create_csv
 import glob
 
 path = "data/"
-data_lst = glob.glob("./data/*.csv")
+data_lst = glob.glob("data/*.csv")
 data_lst.sort(key = os.path.getmtime, reverse = True)
-
+print(data_lst)
 def bar_chart():
     today = date.today()
     current_date = today.strftime("%d-%m-%Y")
-    file_path = path + current_date + ".csv"
+    file_path = data_lst[0]
     # Getting data
     if len(data_lst) == 0:
         create_csv(current_data())
@@ -43,22 +42,18 @@ def bar_chart():
     death = df['death'].tolist()
     del cnf_in[-1], cnf_fn[-1], cured[-1], death[-1]
    
-    # Converting all parameters to tuples
-    cnf_in = tuple([int(x) for x in cnf_in])
-    cnf_fn = tuple([int(x) for x in cnf_fn])
-    cured = tuple([int(x) for x in cured])
-    death = tuple([int(x) for x in death])
-
     # Creating Plot
     fig, ax = plt.subplots()
+    
     index = np.arange(n_groups)
     print(len(index))
-    bar_width = 0.25
+    bar_width = 0.5
     opacity = 0.8
     leg_1 = plt.bar(index, cnf_in, bar_width,
             alpha=opacity,
-            color='#117391',
-            label='Indian Confirmed')
+            color='#05028f',
+            label='Indian Confirmed',
+            )
 
     leg_2 = plt.bar(index, cnf_fn, bar_width,
             alpha=opacity,
@@ -67,7 +62,7 @@ def bar_chart():
 
     leg_3 = plt.bar(index, cured, bar_width,
             alpha=opacity,
-            color='#5cdb9b',
+            color='#00c20f',
             label='Cured')
 
     leg_4 = plt.bar(index, death, bar_width,
@@ -78,12 +73,10 @@ def bar_chart():
     plt.xlabel('People')
     plt.ylabel('Number')
     plt.title('COVID-19 Data (statewise)')
-    plt.xticks(index + bar_width, states)
-    plt.legend()
-
-    plt.tight_layout()
+    plt.xticks(index + bar_width, states, rotation = 90)
+    plt.legend() 
+    plt.savefig("saved_graphs/" + current_date + ".png")
     plt.show()
-
 
 bar_chart()
 

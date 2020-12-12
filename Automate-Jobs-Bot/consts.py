8467d6e09123723687610ef625299c6e663d4e26
Arjun <arj.kashyap123@gmail.com>
2020-12-12 12:56:46 +0530

    final commit

---
+++ Automate-Jobs-Bot/consts.py
@@ -0,0 +1,14 @@
+
+
+"""
+Store constants and credentials
+
+"""
+
+mail = "youremailaddress@xyz.com"
+password = "yourintershallapassword"
+
+app_letter = """
+Some generic Job application letter
+
+"""
